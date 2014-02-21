74eca17d95b724f923143b6b345c1d88b187be0f
glebtv <glebtv@gmail.com>
2014-02-21 20:42:10 +0400

    fix old paperclip

--- app/models/ckeditor/picture.rb
+++ app/models/ckeditor/picture.rb
@@ -3,7 +3,10 @@ class Ckeditor::Picture < Ckeditor::Asset
                             :url  => "/ckeditor_assets/pictures/:id/:style_:basename.:extension",
                             :path => ":rails_root/public/ckeditor_assets/pictures/:id/:style_:basename.:extension",
                             :styles => { :content => '800>', :thumb => '118x100#' }
-  validates_attachment_content_type :data, :content_type => ['image/gif', 'image/jpeg', 'image/jpg', 'image/png'], if: :image?
+  if respond_to?(:validates_attachment_content_type)
+    validates_attachment_content_type :data, :content_type => ['image/gif', 'image/jpeg', 'image/jpg', 'image/png'], if: :image?
+  end
+  
   validates_attachment_size :data, :less_than => 2.megabytes
   validates_attachment_presence :data
 
--- lib/rocket_cms/version.rb
+++ lib/rocket_cms/version.rb
@@ -1,3 +1,3 @@
 module RocketCms
-  VERSION = "0.0.13"
+  VERSION = "0.0.14"
 end
