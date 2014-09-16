12f961184499de267afaf97c2b13978d8dc9c81c
glebtv <glebtv@gmail.com>
2014-09-16 12:19:29 +0400

    update generator template

--- template.rb
+++ template.rb
@@ -468,7 +468,7 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Piudelcibo
+module #{app_name}
   class Application < Rails::Application
     config.generators do |g|
       g.test_framework :rspec
