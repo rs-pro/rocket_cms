a827d2a0ea02ccfebf6c69da00aac0b3bfb1c83e
glebtv <glebtv@gmail.com>
2014-10-09 14:16:17 +0400

    gem update

--- release.sh
+++ release.sh
@@ -2,7 +2,8 @@
 bundle update
 gt 'release gem'
 rake release
-cd mongoid && bundle update && cd .. && cd activerecord && bundle update && cd .. 
+cd mongoid && bundle update && cd ..
+cd activerecord && bundle update && cd .. 
 gt 'gem update'
 cd mongoid && rake release && cd ..
 cd activerecord && rake release && cd ..
