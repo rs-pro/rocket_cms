70e533152f07477a1ffd4f20ff292038788ab514
glebtv <glebtv@gmail.com>
2014-10-10 14:29:25 +0400

    release gem

--- lib/rocket_cms/version.rb
+++ lib/rocket_cms/version.rb
@@ -1,3 +1,3 @@
 module RocketCMS
-  VERSION = "0.5.4"
+  VERSION = "0.5.5"
 end
--- mongoid/lib/rocket_cms_mongoid.rb
+++ mongoid/lib/rocket_cms_mongoid.rb
@@ -3,6 +3,7 @@ require 'glebtv-mongoid-paperclip'
 require 'glebtv-mongoid_nested_set'
 require 'mongoid-audit'
 require 'mongoid_slug'
+require 'manual_slug'
 
 module RocketCMS
   def self.orm
