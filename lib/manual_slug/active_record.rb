b82a36a18b1dcbce28eeca0b655dde719ea2ba4f
glebtv <glebtv@gmail.com>
2014-12-10 16:43:40 +0300

    fix AR slug generation

--- lib/manual_slug/active_record.rb
+++ lib/manual_slug/active_record.rb
@@ -15,7 +15,18 @@ module ManualSlug::ActiveRecord
   module ClassMethods
     def manual_slug(field, options = {}, callback = true)
       friendly_id field, use: [:slugged, :finders]
+      define_method(:should_generate_new_friendly_id?) do
+        slug.blank?
+      end
+
+      skip_callback :validation, :before, :set_slug
+      before_validation do
+        if self.slug.blank?
+          self.send(:set_slug)
+        end
+        true
+      end if callback
     end
   end
 end
-
+ 
--- lib/rocket_cms/version.rb
+++ lib/rocket_cms/version.rb
@@ -1,3 +1,3 @@
 module RocketCMS
-  VERSION = "0.5.23"
+  VERSION = "0.5.24"
 end
