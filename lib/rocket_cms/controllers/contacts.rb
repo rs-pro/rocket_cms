f0e0364ac66a663bb9b80fc66fe6947e1e2ff4a9
glebtv <glebtv@gmail.com>
2015-03-18 15:02:16 +0300

    fix name collision

--- lib/rocket_cms/controllers/contacts.rb
+++ lib/rocket_cms/controllers/contacts.rb
@@ -23,7 +23,7 @@ module RocketCMS
             redirect_after_done
           end
         else
-          render_error
+          render_contacts_error
         end
       end
 
@@ -31,7 +31,7 @@ module RocketCMS
       end
 
       private
-      def render_error
+      def render_contacts_error
         if request.xhr? && process_ajax
           render json: {errors: @contact_message.errors}, status: 422
         else
