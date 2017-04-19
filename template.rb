6edd8d77706f80f74729550771ba88f27899cb0d
glebtv <glebtv@gmail.com>
2017-04-19 13:49:11 +0300

    bump template to 5.1

--- template.rb
+++ template.rb
@@ -14,7 +14,7 @@ remove_file 'Gemfile'
 create_file 'Gemfile' do <<-TEXT
 source 'https://rubygems.org'
 
-gem 'rails', '5.0.1'
+gem 'rails', '5.1.0.rc1'
 #{if mongoid then "gem 'mongoid', '~> 6.1.0'" else "gem 'pg'" end}
 
 gem 'sass'
