c758effd5e16b6e9c5e4c37e7ea66ac330bdb0b6
glebtv <glebtv@gmail.com>
2017-06-16 00:19:09 +0300

    workaround for mongoid 6.2

--- lib/rocket_cms/models/mongoid/page.rb
+++ lib/rocket_cms/models/mongoid/page.rb
@@ -4,13 +4,21 @@ module RocketCMS
       module Page
         extend ActiveSupport::Concern
         included do
-          field :regexp, type: String
+          field :qregexp, type: String
           field :redirect, type: String
           field :content, type: String, localize: RocketCMS.config.localize
           field :fullpath, type: String
           has_and_belongs_to_many :menus, inverse_of: :pages
           acts_as_nested_set
           scope :sorted, -> { asc(:lft) }
+        end
+
+        def regexp=(value)
+          self.qregexp = value
+        end
+
+        def regexp
+          self.qregexp || read_attribute(:regexp)
         end
       end
     end
